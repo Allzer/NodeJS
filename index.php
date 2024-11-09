818febf5ec8961b11f84cba4230704d54a3a298b
Денис <109965453+Allzer@users.noreply.github.com>
2024-11-09 18:22:58 +0300

    условные операторы

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
-    define(constant_name: 'AGE', value: 24);
-    echo AGE;
-
-    $number = '1';
-    echo $number;
-    $bool = false;
-
-    intval($number)
+    $a = 3;
+    if($a==5){
+        echo 'a = 5';
+    }
+    else{
+        echo 'not ravno';
+    }
 ?>
