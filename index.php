b5c57234dd0cae34524dc571f6f0f68e1d1f4c88
Денис <109965453+Allzer@users.noreply.github.com>
2024-11-17 17:00:14 +0300

    added part of function

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
     $title = 'Главная';
     include("blocks\\header.php");
 ?>
+
 <h1>Главная</h1>
 
 <?php
-    require("blocks\\footer.php");
+    $lis = [4,5,6,7,87,9];
+    unset($lis[1]);
+    $lis = array_values($lis);
+    print_r($lis);
 ?>
